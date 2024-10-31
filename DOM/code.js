2fc7036d8dd93bfc48c821de25503c0707d77e74
TIN NGOC <TinVg2918@gmail.com>
2024-10-31 23:01:27 +0700

    21 day DOM

--- DOM/code.js
+++
@@ -1,4 +0,0 @@
-let access = document.getElementById("code9");
-let code = access.innerHTML;
-code = code + " midnight";
-alert(code);
---
+++ DOM/dom.js
@@ -0,0 +1,10 @@
+const allTitles = document.getElementsByClassName("title");
+
+console.log(allTitles);
+console.log(allTitles.length);
+
+for (let i = 0; i < allTitles.length; i++) {
+  console.log(allTitles[i]);
+}
+let firstTitle = document.getElementById("first-title");
+console.log(firstTitle);
